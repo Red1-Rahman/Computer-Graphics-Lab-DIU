f8e036e2e04e5154626734500185e781ec142dcd
Redwan Rahman <redwanrahman2002@outlook.com>
2026-02-21 21:29:52 +0600

    Remove  spaces for screenshots

--- lab2/nameID.py
+++ lab2/nameID.py
@@ -1,6 +1,5 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
-
 def iterate():
     glViewport(0, 0, 500, 500)
     glMatrixMode(GL_PROJECTION)
@@ -8,14 +7,12 @@ def iterate():
     glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-
 def draw_horizontal_line(y):
     glLineWidth(3)
     glBegin(GL_LINES)
     glVertex2f(50, y)
     glVertex2f(450, y)
     glEnd()
-
 def draw_R(x, y, size=20):
     """Draw letter R using lines"""
     h = size
@@ -37,7 +34,6 @@ def draw_R(x, y, size=20):
     glVertex2f(x + w, y + h)
     glVertex2f(x + w, y + h//2)
     glEnd()
-
 def draw_E(x, y, size=20):
     h = size
     w = size // 2
@@ -55,7 +51,6 @@ def draw_E(x, y, size=20):
     glVertex2f(x, y)
     glVertex2f(x + w, y)
     glEnd()
-
 def draw_D(x, y, size=20):
     h = size
     w = size // 2
@@ -71,13 +66,11 @@ def draw_D(x, y, size=20):
     glVertex2f(x + w, y + 5)
     glVertex2f(x, y)
     glEnd()
-
 def draw_1(x, y, size=20):
     glBegin(GL_LINES)
     glVertex2f(x + size//2, y)
     glVertex2f(x + size//2, y + size)
     glEnd()
-
 def draw_2(x, y, size=20):
     w = size
     h = size
@@ -91,7 +84,6 @@ def draw_2(x, y, size=20):
     glVertex2f(x, y)
     glVertex2f(x + w, y)
     glEnd()
-
 def draw_7(x, y, size=20):
     glBegin(GL_LINES)
     glVertex2f(x, y + size)
@@ -99,27 +91,21 @@ def draw_7(x, y, size=20):
     glVertex2f(x + size, y + size)
     glVertex2f(x + size//2, y)
     glEnd()
-
 def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
-
     glColor3f(0.0, 0.0, 1.0)  # blue
-
     # Draw horizontal line
     draw_horizontal_line(250)
-
     # Draw Name "Red" above line
     draw_R(100, 280)
     draw_E(140, 280)
     draw_D(180, 280)
-
     # Draw ID "127" below line
     draw_1(120, 210)
     draw_2(150, 210)
     draw_7(190, 210)
-
     glutSwapBuffers()
 
 glutInit()
